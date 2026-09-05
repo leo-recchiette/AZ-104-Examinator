35da59aaddd6f2ed2eda1eba598093614216e5d1
leo-recchiette <lx2pwnd@gmail.com>
2026-09-05 14:32:02 +0200

    chore: added button to set question font size

--- AZ-104.Examinator.FE/src/main.tsx
+++ AZ-104.Examinator.FE/src/main.tsx
@@ -3,16 +3,19 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./theme/ThemeContext";
 import { SessionProvider } from "./session/SessionContext";
+import { DisplaySettingsProvider } from "./settings/DisplaySettingsContext";
 import { App } from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
-        <SessionProvider>
-          <App />
-        </SessionProvider>
-      </BrowserRouter>
+      <DisplaySettingsProvider>
+        <BrowserRouter>
+          <SessionProvider>
+            <App />
+          </SessionProvider>
+        </BrowserRouter>
+      </DisplaySettingsProvider>
     </ThemeProvider>
   </StrictMode>,
 );
